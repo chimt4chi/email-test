b5263f05b049ceaf827cca26a88885feae61f798
Rohit Singh <rs9990271.edu@gmail.com>
2024-08-02 17:51:27 +0530

    updated ecosystem for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   apps: [
     {
+      name: "email-test", // Adding a name to your app for better identification in PM2
       script: "npm start",
     },
   ],
@@ -12,10 +13,10 @@ module.exports = {
       host: "35.154.222.230",
       ref: "origin/main",
       repo: "https://github.com/chimt4chi/email-test.git",
-      path: "/home/ubuntu",
+      path: "/home/ubuntu/email-test", // Specifying a more precise path
       "pre-deploy-local": "",
       "post-deploy":
-        "source ~/.nvm/nvm.sh npm install && npm run build && pm2 reload ecosystem.config.js --env production",
+        "source ~/.nvm/nvm.sh && nvm use 16.17.0 && npm install && npm run build && pm2 reload ecosystem.config.js --env production",
       "pre-setup": "",
       "ssh-options": "ForwardAgent=yes",
     },
